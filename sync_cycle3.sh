4cffd5355c65ab078215d0819b4d7d77df0442b5
Yuncheng Li (yli) <raingomm@gmail.com>
2015-05-07 00:05:47 -0400

    update README

--- sync_cycle3.sh
+++ sync_cycle3.sh
@@ -12,11 +12,6 @@ fi
 dist_dir=$node:/localdisk/yli-data/caffe-dist
 
 rsync -arv distribute $dist_dir
-
-#mkl
-#rsync -arv /u/qyou/intel/mkl/lib/intel64/*.so $dist_dir/distribute/deps
-#rsync -arv /u/qyou/intel/composer_xe_2013_sp1.2.144/compiler/lib/intel64/*.so $dist_dir/distribute/deps
-
 rsync -arv examples/cifar10 $dist_dir/examples
 rsync -arv examples/mnist $dist_dir/examples
 rsync -arv mpi_env.csh $dist_dir
